a4ee87987d35b211d7b3e33aac6cba985fef351e
Nhat Duy <nhatduytrinha1@gmail.com>
2025-06-02 01:04:26 +0700

    update sendmessageFunction using ASP.NET

--- project_cuoi_ky/home.Designer.cs
+++ project_cuoi_ky/home.Designer.cs
@@ -28,55 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSend = new System.Windows.Forms.Button();
-            this.tbMsg = new System.Windows.Forms.TextBox();
-            this.lvChat = new System.Windows.Forms.ListView();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.txtMessageInput = new System.Windows.Forms.TextBox();
             this.lvInfo = new System.Windows.Forms.ListView();
             this.chatTitleLabel = new System.Windows.Forms.Label();
             this.contactsLabel = new System.Windows.Forms.Label();
             this.userStatusPanel = new System.Windows.Forms.Panel();
-            this.userProfilePicture = new System.Windows.Forms.PictureBox();
-            this.userNameLabel = new System.Windows.Forms.Label();
             this.userStatusLabel = new System.Windows.Forms.Label();
+            this.txtCurrentUserId = new System.Windows.Forms.Label();
+            this.userProfilePicture = new System.Windows.Forms.PictureBox();
+            this.lstMessages = new System.Windows.Forms.ListBox();
+            this.txtCurrentChatroomId = new System.Windows.Forms.TextBox();
             this.userStatusPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.userProfilePicture)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnSend
+            // btnSendMessage
             // 
-            this.btnSend.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(111)))), ((int)(((byte)(111)))));
-            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSend.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSend.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnSend.Location = new System.Drawing.Point(502, 415);
-            this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 26);
-            this.btnSend.TabIndex = 0;
-            this.btnSend.Text = "Send";
-            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSendMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(111)))), ((int)(((byte)(111)))));
+            this.btnSendMessage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSendMessage.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendMessage.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSendMessage.Location = new System.Drawing.Point(502, 415);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(75, 26);
+            this.btnSendMessage.TabIndex = 0;
+            this.btnSendMessage.Text = "Send";
+            this.btnSendMessage.UseVisualStyleBackColor = false;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
             // 
-            // tbMsg
+            // txtMessageInput
             // 
-            this.tbMsg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tbMsg.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbMsg.Location = new System.Drawing.Point(226, 415);
-            this.tbMsg.Multiline = true;
-            this.tbMsg.Name = "tbMsg";
-            this.tbMsg.Size = new System.Drawing.Size(270, 26);
-            this.tbMsg.TabIndex = 1;
-            // 
-            // lvChat
-            // 
-            this.lvChat.BackColor = System.Drawing.Color.White;
-            this.lvChat.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lvChat.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lvChat.HideSelection = false;
-            this.lvChat.Location = new System.Drawing.Point(226, 40);
-            this.lvChat.Name = "lvChat";
-            this.lvChat.Size = new System.Drawing.Size(351, 369);
-            this.lvChat.TabIndex = 2;
-            this.lvChat.UseCompatibleStateImageBehavior = false;
-            this.lvChat.View = System.Windows.Forms.View.List;
+            this.txtMessageInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMessageInput.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessageInput.Location = new System.Drawing.Point(226, 415);
+            this.txtMessageInput.Multiline = true;
+            this.txtMessageInput.Name = "txtMessageInput";
+            this.txtMessageInput.Size = new System.Drawing.Size(270, 26);
+            this.txtMessageInput.TabIndex = 1;
             // 
             // lvInfo
             // 
@@ -98,7 +87,7 @@
             this.chatTitleLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(111)))), ((int)(((byte)(111)))));
             this.chatTitleLabel.Location = new System.Drawing.Point(226, 13);
             this.chatTitleLabel.Name = "chatTitleLabel";
-            this.chatTitleLabel.Size = new System.Drawing.Size(105, 21);
+            this.chatTitleLabel.Size = new System.Drawing.Size(107, 21);
             this.chatTitleLabel.TabIndex = 4;
             this.chatTitleLabel.Text = "Chat Window";
             // 
@@ -109,7 +98,7 @@
             this.contactsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(111)))), ((int)(((byte)(111)))));
             this.contactsLabel.Location = new System.Drawing.Point(13, 13);
             this.contactsLabel.Name = "contactsLabel";
-            this.contactsLabel.Size = new System.Drawing.Size(76, 21);
+            this.contactsLabel.Size = new System.Drawing.Size(74, 21);
             this.contactsLabel.TabIndex = 5;
             this.contactsLabel.Text = "Contacts";
             // 
@@ -118,12 +107,33 @@
             this.userStatusPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
             this.userStatusPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.userStatusPanel.Controls.Add(this.userStatusLabel);
-            this.userStatusPanel.Controls.Add(this.userNameLabel);
+            this.userStatusPanel.Controls.Add(this.txtCurrentUserId);
             this.userStatusPanel.Controls.Add(this.userProfilePicture);
             this.userStatusPanel.Location = new System.Drawing.Point(13, 409);
             this.userStatusPanel.Name = "userStatusPanel";
             this.userStatusPanel.Size = new System.Drawing.Size(207, 32);
             this.userStatusPanel.TabIndex = 6;
+            // 
+            // userStatusLabel
+            // 
+            this.userStatusLabel.AutoSize = true;
+            this.userStatusLabel.Font = new System.Drawing.Font("Yu Gothic UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userStatusLabel.ForeColor = System.Drawing.Color.Green;
+            this.userStatusLabel.Location = new System.Drawing.Point(34, 15);
+            this.userStatusLabel.Name = "userStatusLabel";
+            this.userStatusLabel.Size = new System.Drawing.Size(39, 13);
+            this.userStatusLabel.TabIndex = 2;
+            this.userStatusLabel.Text = "Online";
+            // 
+            // txtCurrentUserId
+            // 
+            this.txtCurrentUserId.AutoSize = true;
+            this.txtCurrentUserId.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCurrentUserId.Location = new System.Drawing.Point(34, 3);
+            this.txtCurrentUserId.Name = "txtCurrentUserId";
+            this.txtCurrentUserId.Size = new System.Drawing.Size(65, 15);
+            this.txtCurrentUserId.TabIndex = 1;
+            this.txtCurrentUserId.Text = "User Name";
             // 
             // userProfilePicture
             // 
@@ -135,26 +145,20 @@
             this.userProfilePicture.TabIndex = 0;
             this.userProfilePicture.TabStop = false;
             // 
-            // userNameLabel
+            // lstMessages
             // 
-            this.userNameLabel.AutoSize = true;
-            this.userNameLabel.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.userNameLabel.Location = new System.Drawing.Point(34, 3);
-            this.userNameLabel.Name = "userNameLabel";
-            this.userNameLabel.Size = new System.Drawing.Size(68, 15);
-            this.userNameLabel.TabIndex = 1;
-            this.userNameLabel.Text = "User Name";
+            this.lstMessages.FormattingEnabled = true;
+            this.lstMessages.Location = new System.Drawing.Point(230, 40);
+            this.lstMessages.Name = "lstMessages";
+            this.lstMessages.Size = new System.Drawing.Size(347, 368);
+            this.lstMessages.TabIndex = 7;
             // 
-            // userStatusLabel
+            // txtCurrentChatroomId
             // 
-            this.userStatusLabel.AutoSize = true;
-            this.userStatusLabel.Font = new System.Drawing.Font("Yu Gothic UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.userStatusLabel.ForeColor = System.Drawing.Color.Green;
-            this.userStatusLabel.Location = new System.Drawing.Point(34, 15);
-            this.userStatusLabel.Name = "userStatusLabel";
-            this.userStatusLabel.Size = new System.Drawing.Size(43, 13);
-            this.userStatusLabel.TabIndex = 2;
-            this.userStatusLabel.Text = "Online";
+            this.txtCurrentChatroomId.Location = new System.Drawing.Point(340, 13);
+            this.txtCurrentChatroomId.Name = "txtCurrentChatroomId";
+            this.txtCurrentChatroomId.Size = new System.Drawing.Size(100, 20);
+            this.txtCurrentChatroomId.TabIndex = 8;
             // 
             // home
             // 
@@ -162,17 +166,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(589, 450);
+            this.Controls.Add(this.txtCurrentChatroomId);
+            this.Controls.Add(this.lstMessages);
             this.Controls.Add(this.userStatusPanel);
             this.Controls.Add(this.contactsLabel);
             this.Controls.Add(this.chatTitleLabel);
             this.Controls.Add(this.lvInfo);
-            this.Controls.Add(this.lvChat);
-            this.Controls.Add(this.tbMsg);
-            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessageInput);
+            this.Controls.Add(this.btnSendMessage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "home";
             this.Opacity = 0.9D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Chat4 - Home";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.userStatusPanel.ResumeLayout(false);
             this.userStatusPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.userProfilePicture)).EndInit();
@@ -183,15 +190,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnSend;
-        private System.Windows.Forms.TextBox tbMsg;
-        private System.Windows.Forms.ListView lvChat;
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.TextBox txtMessageInput;
         private System.Windows.Forms.ListView lvInfo;
         private System.Windows.Forms.Label chatTitleLabel;
         private System.Windows.Forms.Label contactsLabel;
         private System.Windows.Forms.Panel userStatusPanel;
         private System.Windows.Forms.Label userStatusLabel;
-        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label txtCurrentUserId;
         private System.Windows.Forms.PictureBox userProfilePicture;
+        private System.Windows.Forms.ListBox lstMessages;
+        private System.Windows.Forms.TextBox txtCurrentChatroomId;
     }
 }
